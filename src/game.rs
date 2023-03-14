43487a03a25869e15a65198f42d5a45154489fe1
Theodore L'Heureux <theodorelheureux@gmail.com>
2023-03-14 03:23:33 -0400

    feat: revealed tiles adjacent to clicked revealed tile if flag count matches bomb count

--- src/game.rs
+++ src/game.rs
@@ -103,6 +103,7 @@ impl Game {
                 TileType::Bomb => {
                     tile.tile_state = TileState::Exploded;
                     self.reveal_all();
+                    self.game_state = GameState::Lost;
                     return;
                 }
                 TileType::Empty(0) => {
@@ -131,11 +132,77 @@ impl Game {
                     return;
                 }
             },
+            TileState::Revealed => {
+                let bomb_count = match tile.tile_type {
+                    TileType::Empty(bombs) => match bombs {
+                        0 => return,
+                        _ => bombs,
+                    },
+                    _ => return,
+                };
+
+                let mut flags = 0;
+                for x_offset in -1 as isize..2 {
+                    for y_offset in -1 as isize..2 {
+                        if x_offset == 0 && y_offset == 0 {
+                            continue;
+                        }
+
+                        let x = x as isize + x_offset;
+                        let y = y as isize + y_offset;
+
+                        if x >= 0
+                            && x < self.width as isize
+                            && y >= 0
+                            && y < self.height as isize
+                            && self.tiles[y as usize][x as usize].tile_state == TileState::Flagged
+                        {
+                            flags += 1;
+                        }
+                    }
+                }
+
+                if flags == bomb_count {
+                    for x_offset in -1 as isize..2 {
+                        for y_offset in -1 as isize..2 {
+                            if x_offset == 0 && y_offset == 0 {
+                                continue;
+                            }
+
+                            let x = x as isize + x_offset;
+                            let y = y as isize + y_offset;
+
+                            if x >= 0
+                                && x < self.width as isize
+                                && y >= 0
+                                && y < self.height as isize
+                            {
+                                let tile = &mut self.tiles[y as usize][x as usize];
+                                if tile.tile_state == TileState::Unrevealed {
+                                    self.reveal_tile(x as usize, y as usize);
+                                }
+                            }
+                        }
+                    }
+                }   
+            }
             _ => return,
         }
-        // if self.is_won() {
-        //     self.reveal_all();
-        // }
+        if self.is_won() {
+            self.reveal_all();
+            self.game_state = GameState::Won;
+        }
+    }
+
+    pub fn is_won(&self) -> bool {
+        for row in &self.tiles {
+            for tile in row {
+                if tile.tile_type != TileType::Bomb && tile.tile_state != TileState::Revealed {
+                    return false;
+                }
+            }
+        }
+        true
     }
 
     pub fn flag_tile(&mut self, x: usize, y: usize) {
@@ -161,7 +228,7 @@ impl Game {
         if x >= self.width as usize || y >= self.height as usize {
             return;
         }
-        
+
         if self.game_state == GameState::Start {
             self.place_mines(x, y);
         }
@@ -171,11 +238,11 @@ impl Game {
 
     pub fn right_click(&mut self, x_px: i32, y_px: i32) {
         let (x, y) = tile_position(x_px, y_px, self.width, self.height);
-        
+
         if x >= self.width as usize || y >= self.height as usize {
             return;
         }
-        
+
         self.flag_tile(x, y);
     }
 
