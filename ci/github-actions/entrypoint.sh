1d166f137e363f2aaa79c2ef5a4e98905482f1b5
Theodore L'Heureux <theodorelheureux@gmail.com>
2023-03-26 20:22:30 -0400

    ci(release): add compilers deps

--- ci/github-actions/entrypoint.sh
+++ ci/github-actions/entrypoint.sh
@@ -1,43 +1,76 @@
-#!/usr/bin/env bash
-
-# dependencies
-
-sudo apt install -y libglfw3-dev xorg-dev libx11-dev libxss-dev libxxf86vm-dev libxkbfile-dev libxv-dev	gcc-mingw-w64-x86-64 musl
+#!/bin/bash
 
 set -eu
+
+set_output() {
+  echo "$1=$2" >> "$GITHUB_OUTPUT"
+}
+info() {
+  echo "::info::$*"
+}
+warn() {
+  echo "::warning file=entrypoint.sh::$*"
+}
+error() {
+  echo "::error file=entrypoint.sh::$*"
+}
+
+# For backwards compatible also accept environment variable names, but parse all inputs in github
+# action format
+export RUSTTARGET="${INPUT_RUSTTARGET:-${RUSTTARGET:-}}"
+EXTRA_FILES="${INPUT_EXTRA_FILES:-${EXTRA_FILES:-}}"
+# SRC_DIR is handled in build.sh
+ARCHIVE_TYPES="${INPUT_ARCHIVE_TYPES:-${ARCHIVE_TYPES:-}}"
+ARCHIVE_NAME="${INPUT_ARCHIVE_NAME:-${ARCHIVE_NAME:-}}"
+PRE_BUILD="${INPUT_PRE_BUILD:-${PRE_BUILD:-}}"
+POST_BUILD="${INPUT_POST_BUILD:-${POST_BUILD:-}}"
+export MINIFY="${INPUT_MINIFY:-${MINIFY:-}}"
+export TOOLCHAIN_VERSION="${INPUT_TOOLCHAIN_VERSION:-${TOOLCHAIN_VERSION:-}}"
+UPLOAD_MODE="${INPUT_UPLOAD_MODE:-${UPLOAD_MODE:-release}}"
 
 if [ -z "${CMD_PATH+x}" ]; then
   export CMD_PATH=""
 fi
 
-OUTPUT_DIR="output"
+OUTPUT_DIR="/github/workspace/output"
 mkdir -p "$OUTPUT_DIR"
 
+# Link repo
+PROJECT_ROOT="/rust/build/${GITHUB_REPOSITORY}"
+mkdir -p "$PROJECT_ROOT"
+rmdir "$PROJECT_ROOT"
+ln -s "$GITHUB_WORKSPACE" "$PROJECT_ROOT"
+cd "$PROJECT_ROOT"
 
-if ! FILE_LIST=$(ci/github-actions/build.sh "$OUTPUT_DIR"); then
-  echo "::error file=entrypoint.sh::Build failed" >&2
+# Run pre-build script
+if [ -f "$PRE_BUILD" ]; then
+  "./$PRE_BUILD"
+fi
+# Build
+if ! FILE_LIST=$(/build.sh "$OUTPUT_DIR"); then
+  error "Build failed"
   exit 1
+fi
+# Run post-build script
+if [ -f "$POST_BUILD" ]; then
+  "./$POST_BUILD"
 fi
 
 EVENT_DATA=$(cat "$GITHUB_EVENT_PATH")
 echo "$EVENT_DATA" | jq .
 UPLOAD_URL=$(echo "$EVENT_DATA" | jq -r .release.upload_url)
-if [ "$UPLOAD_URL" = "null" ]; then
-  echo "::error file=entrypoint.sh::The event provided did not contain an upload URL, this workflow can only be used with the release event." >&2
-  exit 1
-fi
 UPLOAD_URL=${UPLOAD_URL/\{?name,label\}/}
 RELEASE_NAME=$(echo "$EVENT_DATA" | jq -r .release.tag_name)
 PROJECT_NAME=$(basename "$GITHUB_REPOSITORY")
-NAME="${NAME:-${PROJECT_NAME}_${RELEASE_NAME}}_x86_64-unknown-linux-gnu"
-ARCHIVE_TYPES="${ARCHIVE_TYPES:-"tar.xz"}"
+NAME="${ARCHIVE_NAME:-${PROJECT_NAME}_${RELEASE_NAME}_${RUSTTARGET}}"
+ARCHIVE_TYPES="${ARCHIVE_TYPES:-"zip"}"
 EXTRA_FILES="${EXTRA_FILES:-""}"
 
 if [ -z "${EXTRA_FILES+x}" ]; then
-  echo "::warning file=entrypoint.sh::EXTRA_FILES not set"
+  warn "EXTRA_FILES not set"
 else
   for file in $(echo -n "${EXTRA_FILES}" | tr " " "\n"); do
-    cp "$file" "$OUTPUT_DIR"
+    cp --parents "$file" "$OUTPUT_DIR"
   done
 fi
 
@@ -49,7 +82,7 @@ fi
 
 FILE_LIST=$(echo "${FILE_LIST}" | awk '{$1=$1};1')
 
-echo "::info::Packing files: $FILE_LIST"
+info "Packing files: $FILE_LIST"
 
 for ARCHIVE_TYPE in $ARCHIVE_TYPES; do
   ARCHIVE="tmp.${ARCHIVE_TYPE}"
@@ -60,28 +93,43 @@ for ARCHIVE_TYPE in $ARCHIVE_TYPES; do
       zip -9r $ARCHIVE ${FILE_LIST}
     ;;
 
-    "tar"|"tar.gz"|"tar.bz2"|"tar.xz")
+    "tar"|"tar.gz"|"tar.bz2"|"tar.xz"|"tar.zst")
       tar caf $ARCHIVE ${FILE_LIST}
     ;;
 
     *)
-      echo "::error file=entrypoint.sh::The given archive type '${ARCHIVE_TYPE}' is not supported; please choose one of 'zip' or 'tar.gz'"
+      error "The given archive type '${ARCHIVE_TYPE}' is not supported; please use a supported archive type."
       continue
   esac
 
-  CHECKSUM=$(sha256sum "${ARCHIVE}" | cut -d ' ' -f 1)
+  FILE_NAME="${NAME}.${ARCHIVE/tmp./}"
+  printf "%s %s" "$(sha256sum "${ARCHIVE}" | cut -d ' ' -f 1)" "$FILE_NAME" > "${ARCHIVE}.sha256sum"
+  CHECKSUM_FILE_NAME="${FILE_NAME}.sha256sum"
 
-  curl \
-    -X POST \
-    --data-binary @"${ARCHIVE}" \
-    -H 'Content-Type: application/octet-stream' \
-    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    "${UPLOAD_URL}?name=${NAME}.${ARCHIVE/tmp./}"
+  mv "$ARCHIVE" "$FILE_NAME"
+  mv "${ARCHIVE}.sha256sum" "$CHECKSUM_FILE_NAME"
+  set_output "BUILT_ARCHIVE" "output/${FILE_NAME}"
+  set_output "BUILT_CHECKSUM" "output/${CHECKSUM_FILE_NAME}"
 
-  curl \
-    -X POST \
-    --data "$CHECKSUM" \
-    -H 'Content-Type: text/plain' \
-    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    "${UPLOAD_URL}?name=${NAME}.${ARCHIVE/tmp./}.sha256sum"
+  if [ "$UPLOAD_MODE" = "release" ]; then
+    if [ "$UPLOAD_URL" = "null" ]; then
+      warn "UPLOAD_MODE \"release\" was specified but no URL to upload to could be detected"
+    else
+      curl \
+        --fail-with-body -sS \
+        -X POST \
+        --data-binary @"${FILE_NAME}" \
+        -H 'Content-Type: application/octet-stream' \
+        -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${UPLOAD_URL}?name=${FILE_NAME}"
+
+      curl \
+        --fail-with-body -sS \
+        -X POST \
+        --data-binary @"$CHECKSUM_FILE_NAME" \
+        -H 'Content-Type: text/plain' \
+        -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${UPLOAD_URL}?name=${CHECKSUM_FILE_NAME}"
+    fi
+  fi
 done
